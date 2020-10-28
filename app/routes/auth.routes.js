e7c6afd674e531412e4757dd123808aa93988861
ReemTarek <32741797+ReemTarek@users.noreply.github.com>
2020-10-28 09:50:42 +0200

    Update auth.routes.js

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -7,6 +7,7 @@ module.exports = function(app)
 {
     app.use(function(req,res,next){
         res.header(
+            
             "Access-Control-Allow-Headers",
             "X-access-token, Origin, Content-Type, Accept"
         );
